4078689525debed5cbd7783c05b9a3e021358147
vitalii-smahlenko <smaglenko@gmail.com>
2023-10-25 09:42:07 +0300

    Refactor controller according to Web API design best practices

--- src/main/java/com/gmail/smaglenko/randompredictionsgenerator/controller/PredictionController.java
+++ src/main/java/com/gmail/smaglenko/randompredictionsgenerator/controller/PredictionController.java
@@ -13,19 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/prediction")
+@RequestMapping("/predictions")
 @RequiredArgsConstructor
 public class PredictionController {
     private final PredictionService predictionService;
     private final PredictionDtoMapper predictionDtoMapper;
 
-    @PostMapping("/create")
+    @PostMapping()
     public PredictionDto createPrediction(@RequestBody PredictionDto dto) {
         return predictionDtoMapper
                 .mapToDto(predictionService.add(predictionDtoMapper.mapToModel(dto)));
     }
 
-    @GetMapping("/get-prediction-for-today")
+    @GetMapping("/get-random")
     public ResponseEntity<String> getPrediction() {
         Prediction randomPrediction = predictionService.getRandomPrediction();
         return ResponseEntity.ok(randomPrediction.getContent());
@@ -33,6 +33,6 @@ public class PredictionController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello world!");
+        return ResponseEntity.ok("Hello random-predictions-generator app!!!");
     }
 }
