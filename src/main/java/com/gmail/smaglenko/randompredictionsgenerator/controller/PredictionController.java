56d9dde1bb92eb61e58af18b023624435006d53c
vitalii.smahlenko <smaglenko@gmail.com>
2023-12-28 22:02:29 +0200

    Create HelloController

--- src/main/java/com/gmail/smaglenko/randompredictionsgenerator/controller/PredictionController.java
+++ src/main/java/com/gmail/smaglenko/randompredictionsgenerator/controller/PredictionController.java
@@ -30,10 +30,4 @@ public class PredictionController {
         Prediction randomPrediction = predictionService.getRandomPrediction();
         return ResponseEntity.ok(randomPrediction.getContent());
     }
-
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
-
-        return ResponseEntity.ok("Hello my friend!!!");
-    }
 }
